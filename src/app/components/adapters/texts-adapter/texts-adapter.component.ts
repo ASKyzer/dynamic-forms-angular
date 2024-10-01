bb4adc8070d03f6c0ccd5f2cc2f1a46a164db58e
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-01 22:01:47 +0200

    create text adapter component

---
+++ src/app/components/adapters/texts-adapter/texts-adapter.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export interface TextsAdapterConfig {
+  content?: string;
+  listTitle?: string;
+  listItems?: string[];
+  isBold?: boolean;
+  isItalic?: boolean;
+  isUnderline?: boolean;
+  type: 'h1' | 'h2' | 'h3' | 'p' | 'ul' | 'ol' | 'blockquote';
+}
+
+@Component({
+  selector: 'app-texts-adapter',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './texts-adapter.component.html',
+})
+export class TextsAdapterComponent {
+  @Input() contentConfig!: TextsAdapterConfig;
+  @Input() parentGroup: any;
+
+  getFontStyle() {
+    return {
+      'font-bold': this.contentConfig.isBold,
+      italic: this.contentConfig.isItalic,
+      underline: this.contentConfig.isUnderline,
+    };
+  }
+}
