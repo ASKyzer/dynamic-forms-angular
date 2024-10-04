f6e48a619dbd6e3b0fe069f003e65fc14a868edb
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-04 11:54:16 +0200

    make row selection visible only if the input type is textarea

---
+++ src/app/constants/input-builder.constant.ts
@@ -0,0 +1,236 @@
+export const INPUT_BUILDER_CONFIG = {
+  sections: [
+    {
+      rows: [
+        {
+          fields: [
+            {
+              adapterType: 'dropdown',
+              config: {
+                controlName: 'fieldType',
+                label: 'Field Type',
+                isRequired: true,
+                options: [
+                  {
+                    label: 'Text',
+                    value: 'text',
+                  },
+                  {
+                    label: 'Email',
+                    value: 'email',
+                  },
+                  {
+                    label: 'Number',
+                    value: 'number',
+                  },
+                  {
+                    label: 'Tel',
+                    value: 'tel',
+                  },
+                  {
+                    label: 'Textarea',
+                    value: 'textarea',
+                  },
+                  {
+                    label: 'Date',
+                    value: 'date',
+                  },
+                  {
+                    label: 'Password',
+                    value: 'password',
+                  },
+                ],
+                validation: [
+                  {
+                    type: 'required',
+                  },
+                ],
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'label',
+                label: 'Label',
+                isRequired: true,
+                placeholder: 'Enter label',
+                type: 'text',
+                validation: [
+                  {
+                    type: 'required',
+                  },
+                ],
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'controlName',
+                label: 'Control Name',
+                isRequired: true,
+                placeholder: 'Enter control name in camelCase',
+                type: 'text',
+                validation: [
+                  {
+                    type: 'required',
+                  },
+                ],
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'placeholder',
+                label: 'Placeholder',
+                placeholder: 'Enter placeholder (optional)',
+                type: 'text',
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'errorMessage',
+                label: 'Error Message',
+                placeholder:
+                  'Enter a custom error message, leave blank for default (optional)',
+                type: 'text',
+              },
+            },
+          ],
+        },
+        {
+          conditions: {
+            field: 'fieldType',
+            operator: '==',
+            value: 'textarea',
+          },
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'rows',
+                label: 'Textarea Rows',
+                placeholder:
+                  'Enter number of rows if different from 4 (optional)',
+                type: 'number',
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'texts',
+              config: {
+                content: 'Add validation',
+                isBold: true,
+                type: 'p',
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'checkbox',
+              config: {
+                controlName: 'checkbox',
+                option: {
+                  label: 'This field required',
+                  value: 'true',
+                },
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'checkbox',
+              config: {
+                controlName: 'emailValidation',
+                option: {
+                  label: 'This field need to be a valid email',
+                  value: 'true',
+                },
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'minLength',
+                type: 'number',
+                label: 'Min Length',
+                placeholder: 'Enter min length (optional)',
+              },
+            },
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'maxLength',
+                type: 'number',
+                label: 'Max Length',
+                placeholder: 'Enter max length (optional)',
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'min',
+                type: 'number',
+                label: 'Min',
+                placeholder: 'Enter min (optional)',
+              },
+            },
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'max',
+                type: 'number',
+                label: 'Max',
+                placeholder: 'Enter max (optional)',
+              },
+            },
+          ],
+        },
+        {
+          fields: [
+            {
+              adapterType: 'input',
+              config: {
+                controlName: 'pattern',
+                type: 'text',
+                label: 'Pattern',
+                placeholder: 'Enter pattern regex (optional)',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
