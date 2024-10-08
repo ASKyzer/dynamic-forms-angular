9afe41f3cd034abe3ccc3692a2d107414895fafe
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-08 19:16:29 +0200

    remove group checkboxes

--- src/app/components/adapters/checkbox-adapter/checkbox-adapter.component.ts
+++ src/app/components/adapters/checkbox-adapter/checkbox-adapter.component.ts
@@ -5,14 +5,7 @@ import {
   NO_ERRORS_SCHEMA,
   OnInit,
 } from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormControl,
-  FormGroup,
-  ValidatorFn,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {
   CheckboxComponent,
   CheckboxOption,
@@ -51,54 +44,12 @@ export class CheckboxAdapterComponent implements OnInit {
     this.initializeFormControl();
   }
 
-  atLeastOneCheckboxCheckedValidator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-      if (control instanceof FormArray) {
-        const checked = control.controls.some(
-          (checkbox) => checkbox.value === true
-        );
-
-        return checked || !this.contentConfig.isRequired
-          ? null
-          : { required: true };
-      }
-      // If it's not a FormArray, return null (no error)
-      return null;
-    };
-  }
-
-  getValidators() {
-    return this.contentConfig.validation.map((validation: any) => {
-      switch (validation.type) {
-        case 'requiredTrue':
-          return Validators.requiredTrue;
-        default:
-          return null;
-      }
-    });
-  }
-
   initializeFormControl() {
-    if (this.contentConfig.isGroup) {
-      const formArray = this.getFormArray(this.contentConfig.options);
-      this.parentForm.addControl(this.contentConfig.controlName, formArray);
-      formArray.setValidators(this.atLeastOneCheckboxCheckedValidator());
-      formArray.updateValueAndValidity();
-    } else {
-      this.parentForm.addControl(
-        this.contentConfig.controlName,
-        new FormControl(
-          this.contentConfig.option.checked || false,
-          this.contentConfig.isRequired ? Validators.requiredTrue : null
-        )
-      );
-    }
-  }
-
-  getFormArray(options: CheckboxOption[]) {
-    return new FormArray(
-      options.map(
-        (option: CheckboxOption) => new FormControl(option.checked || false)
+    this.parentForm.addControl(
+      this.contentConfig.controlName,
+      new FormControl(
+        this.contentConfig.option.checked || false,
+        this.contentConfig.isRequired ? Validators.requiredTrue : null
       )
     );
   }
--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,11 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 export interface CheckboxOption {
   checked?: boolean;
@@ -23,29 +18,16 @@ export class CheckboxComponent {
   @Input() control: FormControl = new FormControl();
   @Input() controlName!: string;
   @Input() errorMessage = 'Please select at least one option';
-  @Input() groupLabel!: string;
-  @Input() groupName!: string;
   @Input() isDisabled = false;
-  @Input() isGroup = false;
   @Input() isRequired = false;
   @Input() label!: string;
   @Input() margin = '4';
   @Input() option!: CheckboxOption;
-  @Input() options!: CheckboxOption[];
   @Input() parentForm!: FormGroup;
 
   ngOnInit() {
     if (!this.control) {
       console.error('FormControl is required for InputFieldComponent');
     }
-    if (!this.parentForm.get(this.controlName)) {
-      this.parentForm.setControl(this.controlName, new FormArray([]));
-    }
-  }
-
-  onCheckboxChange(event: any, index: number) {
-    const formArray = this.parentForm.get(this.controlName) as FormArray;
-    formArray.at(index).setValue(event?.target?.checked);
-    formArray.updateValueAndValidity();
   }
 }
