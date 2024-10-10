21b24437e36c5928d25f59372aec14f6809b7191
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-10 19:37:24 +0200

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalComponent } from './components/modal/modal.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ModalComponent],
-  template: `<router-outlet></router-outlet><app-modal></app-modal>`,
+  imports: [RouterOutlet, ModalComponent, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <main class="mt-16">
+      <router-outlet></router-outlet>
+    </main>
+    <app-modal></app-modal>
+  `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {}
